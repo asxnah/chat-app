5c3fc139d52913eb3ed5e850e09507050f960020
아수나 <105264974+asxnah@users.noreply.github.com>
2025-07-18 17:42:17 +0500

    feat: mobile header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,36 @@
+import type { ReactElement } from 'react';
+import { ChevronLeft } from '../../assets/icons/ChevronLeft';
+import styles from './styles.module.css';
+
+interface HeaderProps {
+	heading: string;
+	onChevronClick: () => void;
+
+	avatar?: string;
+	extension?: ReactElement;
+	onHeadingClick?: () => void;
+	onExtensionClick?: () => void;
+}
+
+export const Header = ({
+	heading,
+	onChevronClick,
+	avatar,
+	extension,
+	onHeadingClick,
+	onExtensionClick,
+}: HeaderProps) => {
+	return (
+		<header className={`${styles.header} ${avatar && styles.hasAvatar}`}>
+			<button onClick={onChevronClick}>
+				<ChevronLeft />
+			</button>
+			<div>
+				{avatar && <img src={avatar} alt="contact avatar" />}
+				<h1>{heading}</h1>
+				<button onClick={onHeadingClick}></button>
+			</div>
+			{extension && <button onClick={onExtensionClick}>{extension}</button>}
+		</header>
+	);
+};
