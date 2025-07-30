24e9c859d32368d24bae691b916366b9a3b7e2f8
sohee <cherepanova.so04@gmail.com>
2025-07-30 16:46:17 +0500

    refactor: more data

--- src/mockData/contacts.ts
+++ src/mockData/contacts.ts
@@ -3,7 +3,7 @@ export const fetchedContacts = [
 		id: '1',
 		name: 'Cutie𖦹𖦹',
 		avatar:
-			'https://i.pinimg.com/1200x/ae/6c/74/ae6c748ab01cd6696ef77c6ba27ed6f2.jpg',
+			'https://i.pinimg.com/736x/d2/e8/ef/d2e8efc1f720719e4be8d021b68b1187.jpg',
 		email: 'email@example.com',
 		notifs: true,
 	},
@@ -11,7 +11,63 @@ export const fetchedContacts = [
 		id: '2',
 		name: 'Bunny 🐇',
 		avatar:
-			'https://i.pinimg.com/736x/8d/94/44/8d9444611dab6bdec74ea00df0ec59a2.jpg',
+			'https://i.pinimg.com/736x/c4/4d/c5/c44dc53bb7da5b44a2f77ecaa4564143.jpg',
+		email: 'email@example.com',
+		notifs: true,
+	},
+	{
+		id: '3',
+		name: 'Milk ♡',
+		avatar:
+			'https://i.pinimg.com/736x/59/42/73/59427305a89c1b51084e5b7942ecfbff.jpg',
+		email: 'email@example.com',
+		notifs: true,
+	},
+	{
+		id: '4',
+		name: 'Angel ⭒',
+		avatar:
+			'https://i.pinimg.com/736x/43/07/a7/4307a75708d6cb085dce96f847736042.jpg',
+		email: 'email@example.com',
+		notifs: true,
+	},
+	{
+		id: '5',
+		name: 'Mochi 𖧷',
+		avatar:
+			'https://i.pinimg.com/736x/e3/50/78/e350787155c69beec22bd876d83ac47d.jpg',
+		email: 'email@example.com',
+		notifs: true,
+	},
+	{
+		id: '6',
+		name: 'Snowbun 🐇',
+		avatar:
+			'https://i.pinimg.com/1200x/f5/95/88/f595881a33f410ff12b713471e3c118d.jpg',
+		email: 'email@example.com',
+		notifs: true,
+	},
+	{
+		id: '7',
+		name: 'Cherryblossom 🌸',
+		avatar:
+			'https://i.pinimg.com/1200x/fc/37/a0/fc37a04dda8de9c657d558d452d8fabc.jpg',
+		email: 'email@example.com',
+		notifs: true,
+	},
+	{
+		id: '8',
+		name: 'Cotton ♡',
+		avatar:
+			'https://i.pinimg.com/736x/02/21/73/02217357a492330e7eeade09de7f7779.jpg',
+		email: 'email@example.com',
+		notifs: true,
+	},
+	{
+		id: '10',
+		name: 'Peachmilk ⭒',
+		avatar:
+			'https://i.pinimg.com/736x/f2/95/c4/f295c4ed55fc54b7e064ca38619c75cf.jpg',
 		email: 'email@example.com',
 		notifs: true,
 	},
