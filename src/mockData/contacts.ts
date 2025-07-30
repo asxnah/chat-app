ae024b3af53cd43a87acddbcc0861a0819ce97be
sohee <cherepanova.so04@gmail.com>
2025-07-30 17:12:56 +0500

    refactor: emails

--- src/mockData/contacts.ts
+++ src/mockData/contacts.ts
@@ -4,7 +4,7 @@ export const fetchedContacts = [
 		name: 'Cutie𖦹𖦹',
 		avatar:
 			'https://i.pinimg.com/736x/d2/e8/ef/d2e8efc1f720719e4be8d021b68b1187.jpg',
-		email: 'email@example.com',
+		email: 'cutie@example.com',
 		notifs: true,
 	},
 	{
@@ -12,7 +12,7 @@ export const fetchedContacts = [
 		name: 'Bunny 🐇',
 		avatar:
 			'https://i.pinimg.com/736x/c4/4d/c5/c44dc53bb7da5b44a2f77ecaa4564143.jpg',
-		email: 'email@example.com',
+		email: 'bunny@example.com',
 		notifs: true,
 	},
 	{
@@ -20,7 +20,7 @@ export const fetchedContacts = [
 		name: 'Milk ♡',
 		avatar:
 			'https://i.pinimg.com/736x/59/42/73/59427305a89c1b51084e5b7942ecfbff.jpg',
-		email: 'email@example.com',
+		email: 'milk@example.com',
 		notifs: true,
 	},
 	{
@@ -28,7 +28,7 @@ export const fetchedContacts = [
 		name: 'Angel ⭒',
 		avatar:
 			'https://i.pinimg.com/736x/43/07/a7/4307a75708d6cb085dce96f847736042.jpg',
-		email: 'email@example.com',
+		email: 'angel@example.com',
 		notifs: true,
 	},
 	{
@@ -36,7 +36,7 @@ export const fetchedContacts = [
 		name: 'Mochi 𖧷',
 		avatar:
 			'https://i.pinimg.com/736x/e3/50/78/e350787155c69beec22bd876d83ac47d.jpg',
-		email: 'email@example.com',
+		email: 'mochi@example.com',
 		notifs: true,
 	},
 	{
@@ -44,7 +44,7 @@ export const fetchedContacts = [
 		name: 'Snowbun 🐇',
 		avatar:
 			'https://i.pinimg.com/1200x/f5/95/88/f595881a33f410ff12b713471e3c118d.jpg',
-		email: 'email@example.com',
+		email: 'snowbun@example.com',
 		notifs: true,
 	},
 	{
@@ -52,7 +52,7 @@ export const fetchedContacts = [
 		name: 'Cherryblossom 🌸',
 		avatar:
 			'https://i.pinimg.com/1200x/fc/37/a0/fc37a04dda8de9c657d558d452d8fabc.jpg',
-		email: 'email@example.com',
+		email: 'cherryblossom@example.com',
 		notifs: true,
 	},
 	{
@@ -60,7 +60,7 @@ export const fetchedContacts = [
 		name: 'Cotton ♡',
 		avatar:
 			'https://i.pinimg.com/736x/02/21/73/02217357a492330e7eeade09de7f7779.jpg',
-		email: 'email@example.com',
+		email: 'cotton@example.com',
 		notifs: true,
 	},
 	{
@@ -68,7 +68,7 @@ export const fetchedContacts = [
 		name: 'Peachmilk ⭒',
 		avatar:
 			'https://i.pinimg.com/736x/f2/95/c4/f295c4ed55fc54b7e064ca38619c75cf.jpg',
-		email: 'email@example.com',
+		email: 'peachmilk@example.com',
 		notifs: true,
 	},
 ];
