f86df94b1739e4bae135d7683bc1eb3c528e11a7
sohee <cherepanova.so04@gmail.com>
2025-08-15 18:48:52 +0500

    feat: роуты мобилок

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { TabBar } from './components/TabBar/TabBar';
 import Test from './pages/Test/Test';
 import Auth from './pages/Auth/Auth';
 import Contacts from './pages/Contacts/Contacts';
-import ContactAdd from './pages/ContactAdd/ContactAdd';
-import ContactEdit from './pages/ContactEdit/ContactEdit';
+import BasicInfoFormPage from './pages/mobile/BasicInfoFormPage/BasicInfoFormPage';
+import Contact from './pages/mobile/Contact/Contact';
+// import useWindowWidth from './hooks/useWindowWidth';
 
 function App() {
+	// const width = useWindowWidth();
 	return (
 		<BrowserRouter>
-			<TabBar />
 			<Routes>
 				<Route path="/" element={<Test />} />
 				<Route path="/auth" element={<Auth />} />
 				<Route path="/contacts" element={<Contacts />} />
-				<Route path="/contacts/add" element={<ContactAdd />} />
-				<Route path="/contacts/edit" element={<ContactEdit />} />
+				<Route path="/contacts/create" element={<BasicInfoFormPage />} />
+				<Route path="/contact/:id" element={<Contact />} />
+				<Route path="/contact/edit/:id" element={<BasicInfoFormPage />} />
+				<Route path="/settings/profile/edit" element={<BasicInfoFormPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
