cd58cc46da157cbccd7cdc6768148dc9ddf9127b
sohee <cherepanova.so04@gmail.com>
2025-08-15 17:20:52 +0500

    del: отказ от идеи отдельных одинаковых мобильных страниц

--- src/pages/ContactAdd/ContactAdd.tsx
+++
@@ -1,7 +0,0 @@
-import s from './styles.module.css';
-
-const ContactAdd = () => {
-	return <main className={s.main}></main>;
-};
-
-export default ContactAdd;
--- src/pages/ContactEdit/ContactEdit.tsx
+++
@@ -1,7 +0,0 @@
-import s from './styles.module.css';
-
-const ContactEdit = () => {
-	return <main className={s.main}></main>;
-};
-
-export default ContactEdit;
