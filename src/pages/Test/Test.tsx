8f86930042070f333a98938658a4798e3d9a0517
sohee <cherepanova.so04@gmail.com>
2025-08-18 17:36:12 +0500

    feat: header

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -9,6 +9,7 @@ import { Confirm } from '../../uikit/Confirm/Confirm';
 import { Form } from '../../components/Form/Form';
 
 import s from './styles.module.css';
+import { Header } from '../../components/Header/Header';
 
 const Test = () => {
 	const navigate = useNavigate();
@@ -19,101 +20,104 @@ const Test = () => {
 	const [nameValue, setName] = useState('');
 
 	return (
-		<main className={s.main}>
-			<section className={s.main__section}>
-				<UserInfo
-					type="link"
-					name="Link somewhere"
-					onClick={() => navigate('/')}
-				/>
-				<UserInfo
-					type="contact"
-					name="Yuuki Asuna"
-					avatar="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fswordartonline%2Fimages%2F0%2F06%2FAsuna_with_Yui_Biprobe.png%2Frevision%2Flatest%3Fcb%3D20141220180221&f=1&nofb=1&ipt=846f0f8e2eb21ba64bfdd02f854a54d2238d0a385afcd2b9056e9c220f8a8502"
-				/>
-				<UserInfo
-					type="profile"
-					name="Yuuki Asuna"
-					content="email@example.com"
-					avatar="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fswordartonline%2Fimages%2F0%2F06%2FAsuna_with_Yui_Biprobe.png%2Frevision%2Flatest%3Fcb%3D20141220180221&f=1&nofb=1&ipt=846f0f8e2eb21ba64bfdd02f854a54d2238d0a385afcd2b9056e9c220f8a8502"
-				/>
-				<UserInfo
-					type="message"
-					name="Yuuki Asuna"
-					status="New chat"
-					date="Now"
-					avatar="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fswordartonline%2Fimages%2F0%2F06%2FAsuna_with_Yui_Biprobe.png%2Frevision%2Flatest%3Fcb%3D20141220180221&f=1&nofb=1&ipt=846f0f8e2eb21ba64bfdd02f854a54d2238d0a385afcd2b9056e9c220f8a8502"
-				/>
-				<UserInfo
-					type="message"
-					name="Yuuki Asuna"
-					content="Yes, I'll be there"
-					date="11:30"
-					avatar="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fswordartonline%2Fimages%2F0%2F06%2FAsuna_with_Yui_Biprobe.png%2Frevision%2Flatest%3Fcb%3D20141220180221&f=1&nofb=1&ipt=846f0f8e2eb21ba64bfdd02f854a54d2238d0a385afcd2b9056e9c220f8a8502"
-				/>
-				<UserInfo
-					type="message"
-					name="Yuuki Asuna"
-					content="Selected chat"
-					date="11:30"
-					avatar="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fswordartonline%2Fimages%2F0%2F06%2FAsuna_with_Yui_Biprobe.png%2Frevision%2Flatest%3Fcb%3D20141220180221&f=1&nofb=1&ipt=846f0f8e2eb21ba64bfdd02f854a54d2238d0a385afcd2b9056e9c220f8a8502"
-					selected={true}
-				/>
-				<UserInfo
-					type="message"
-					name="Yuuki Asuna"
-					status="Typing..."
-					date="10:15"
-					avatar="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fswordartonline%2Fimages%2F0%2F06%2FAsuna_with_Yui_Biprobe.png%2Frevision%2Flatest%3Fcb%3D20141220180221&f=1&nofb=1&ipt=846f0f8e2eb21ba64bfdd02f854a54d2238d0a385afcd2b9056e9c220f8a8502"
-				/>
-				<UserInfo
-					type="message"
-					name="Yuuki Asuna"
-					content="When can we meet?"
-					date="Yesterday"
-					avatar="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fswordartonline%2Fimages%2F0%2F06%2FAsuna_with_Yui_Biprobe.png%2Frevision%2Flatest%3Fcb%3D20141220180221&f=1&nofb=1&ipt=846f0f8e2eb21ba64bfdd02f854a54d2238d0a385afcd2b9056e9c220f8a8502"
-					counter={3}
-				/>
-			</section>
-
-			<section
-				className={s.main__section}
-				style={{ display: 'flex', gap: '16px' }}
-			>
-				<Button content="Button" onClick={() => alert('clicked')} />
-				<Input
-					placeholder="Placeholder"
-					value={value}
-					onChange={(e) => setValue(e.target.value)}
-				/>
-				<Toggler
-					content="Toggler"
-					checked={checked}
-					onToggle={() => setChecked(!checked)}
-				/>
-				<div
-					style={{
-						padding: '8px',
-						borderRadius: '16px',
-						backgroundColor: 'var(--lightgrey)',
-					}}
-				>
-					<Confirm
-						content="Confirm?"
-						onConfirm={() => alert('confirmed')}
-						onDecline={() => alert('declined')}
+		<>
+			<Header heading="Test" onChevronClick={() => {}} />
+			<main className={s.main}>
+				<section className={s.main__section}>
+					<UserInfo
+						type="link"
+						name="Link somewhere"
+						onClick={() => navigate('/')}
 					/>
-				</div>
-				<Form
-					buttonText="Submit"
-					nameValue={nameValue}
-					emailValue={emailValue}
-					onNameChange={(e) => setName(e.target.value)}
-					onEmailChange={(e) => setEmail(e.target.value)}
-					onSubmit={() => alert('Submitted')}
-				/>
-			</section>
-		</main>
+					<UserInfo
+						type="contact"
+						name="Yuuki Asuna"
+						avatar="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fswordartonline%2Fimages%2F0%2F06%2FAsuna_with_Yui_Biprobe.png%2Frevision%2Flatest%3Fcb%3D20141220180221&f=1&nofb=1&ipt=846f0f8e2eb21ba64bfdd02f854a54d2238d0a385afcd2b9056e9c220f8a8502"
+					/>
+					<UserInfo
+						type="profile"
+						name="Yuuki Asuna"
+						content="email@example.com"
+						avatar="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fswordartonline%2Fimages%2F0%2F06%2FAsuna_with_Yui_Biprobe.png%2Frevision%2Flatest%3Fcb%3D20141220180221&f=1&nofb=1&ipt=846f0f8e2eb21ba64bfdd02f854a54d2238d0a385afcd2b9056e9c220f8a8502"
+					/>
+					<UserInfo
+						type="message"
+						name="Yuuki Asuna"
+						status="New chat"
+						date="Now"
+						avatar="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fswordartonline%2Fimages%2F0%2F06%2FAsuna_with_Yui_Biprobe.png%2Frevision%2Flatest%3Fcb%3D20141220180221&f=1&nofb=1&ipt=846f0f8e2eb21ba64bfdd02f854a54d2238d0a385afcd2b9056e9c220f8a8502"
+					/>
+					<UserInfo
+						type="message"
+						name="Yuuki Asuna"
+						content="Yes, I'll be there"
+						date="11:30"
+						avatar="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fswordartonline%2Fimages%2F0%2F06%2FAsuna_with_Yui_Biprobe.png%2Frevision%2Flatest%3Fcb%3D20141220180221&f=1&nofb=1&ipt=846f0f8e2eb21ba64bfdd02f854a54d2238d0a385afcd2b9056e9c220f8a8502"
+					/>
+					<UserInfo
+						type="message"
+						name="Yuuki Asuna"
+						content="Selected chat"
+						date="11:30"
+						avatar="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fswordartonline%2Fimages%2F0%2F06%2FAsuna_with_Yui_Biprobe.png%2Frevision%2Flatest%3Fcb%3D20141220180221&f=1&nofb=1&ipt=846f0f8e2eb21ba64bfdd02f854a54d2238d0a385afcd2b9056e9c220f8a8502"
+						selected={true}
+					/>
+					<UserInfo
+						type="message"
+						name="Yuuki Asuna"
+						status="Typing..."
+						date="10:15"
+						avatar="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fswordartonline%2Fimages%2F0%2F06%2FAsuna_with_Yui_Biprobe.png%2Frevision%2Flatest%3Fcb%3D20141220180221&f=1&nofb=1&ipt=846f0f8e2eb21ba64bfdd02f854a54d2238d0a385afcd2b9056e9c220f8a8502"
+					/>
+					<UserInfo
+						type="message"
+						name="Yuuki Asuna"
+						content="When can we meet?"
+						date="Yesterday"
+						avatar="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fswordartonline%2Fimages%2F0%2F06%2FAsuna_with_Yui_Biprobe.png%2Frevision%2Flatest%3Fcb%3D20141220180221&f=1&nofb=1&ipt=846f0f8e2eb21ba64bfdd02f854a54d2238d0a385afcd2b9056e9c220f8a8502"
+						counter={3}
+					/>
+				</section>
+
+				<section
+					className={s.main__section}
+					style={{ display: 'flex', gap: '16px' }}
+				>
+					<Button content="Button" onClick={() => alert('clicked')} />
+					<Input
+						placeholder="Placeholder"
+						value={value}
+						onChange={(e) => setValue(e.target.value)}
+					/>
+					<Toggler
+						content="Toggler"
+						checked={checked}
+						onToggle={() => setChecked(!checked)}
+					/>
+					<div
+						style={{
+							padding: '8px',
+							borderRadius: '16px',
+							backgroundColor: 'var(--lightgrey)',
+						}}
+					>
+						<Confirm
+							content="Confirm?"
+							onConfirm={() => alert('confirmed')}
+							onDecline={() => alert('declined')}
+						/>
+					</div>
+					<Form
+						buttonText="Submit"
+						nameValue={nameValue}
+						emailValue={emailValue}
+						onNameChange={(e) => setName(e.target.value)}
+						onEmailChange={(e) => setEmail(e.target.value)}
+						onSubmit={() => alert('Submitted')}
+					/>
+				</section>
+			</main>
+		</>
 	);
 };
 
