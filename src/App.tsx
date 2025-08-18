88144a702831f01374fd56c8aba457f4aa184a64
sohee <cherepanova.so04@gmail.com>
2025-08-18 17:32:59 +0500

    feat: tabbar if w>880

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import useWindowWidth from './hooks/useWindowWidth';
 import Test from './pages/Test/Test';
 import Auth from './pages/Auth/Auth';
 import Contacts from './pages/Contacts/Contacts';
 import BasicInfoFormPage from './pages/mobile/BasicInfoFormPage/BasicInfoFormPage';
 import Contact from './pages/mobile/Contact/Contact';
-// import useWindowWidth from './hooks/useWindowWidth';
+import { TabBar } from './components/TabBar/TabBar';
 
 function App() {
-	// const width = useWindowWidth();
+	const width = useWindowWidth();
 	return (
 		<BrowserRouter>
+			{width > 800 && <TabBar />}
 			<Routes>
 				<Route path="/" element={<Test />} />
 				<Route path="/auth" element={<Auth />} />
