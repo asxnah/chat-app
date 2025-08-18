4bec9771ab11cd71717276e95563e736c272756f
sohee <cherepanova.so04@gmail.com>
2025-08-18 18:10:43 +0500

    feat: header

--- src/pages/mobile/Contact/Contact.tsx
+++ src/pages/mobile/Contact/Contact.tsx
@@ -5,10 +5,11 @@ import { Toggler } from '../../../uikit/Toggler/Toggler';
 import s from './styles.module.css';
 
 import fetchedContacts from '../../../mockData/contacts.json';
+import { Header } from '../../../components/Header/Header';
 
 const Contact = () => {
 	const navigate = useNavigate();
-  const { id } = useParams<{ id: string }>();
+	const { id } = useParams<{ id: string }>();
 
 	const [checked, setChecked] = useState<boolean>(true);
 	const [contact, setContact] = useState<{
@@ -42,24 +43,27 @@ const Contact = () => {
 	}, [id]);
 
 	return (
-		<main className={s.main}>
-			<UserInfo
-				type="profile"
-				avatar={contact?.avatar}
-				name={contact?.name}
-				content={contact?.email}
-				onClick={() => navigate(`/contact/edit/${contact?.id}`)}
-			/>
-			<UserInfo type="link" name="Write a message" />
-			<Toggler
-				content="Notifications"
-				checked={checked}
-				onToggle={() => setChecked(!checked)}
-			/>
-			<button className={s.deleteButton} onClick={deleteContact}>
-				Delete contact
-			</button>
-		</main>
+		<>
+			<Header heading="Profile" onChevronClick={() => navigate('/contacts')} />
+			<main className={s.main}>
+				<UserInfo
+					type="profile"
+					avatar={contact?.avatar}
+					name={contact?.name}
+					content={contact?.email}
+					onClick={() => navigate(`/contact/edit/${contact?.id}`)}
+				/>
+				<UserInfo type="link" name="Write a message" />
+				<Toggler
+					content="Notifications"
+					checked={checked}
+					onToggle={() => setChecked(!checked)}
+				/>
+				<button className={s.deleteButton} onClick={deleteContact}>
+					Delete contact
+				</button>
+			</main>
+		</>
 	);
 };
 
