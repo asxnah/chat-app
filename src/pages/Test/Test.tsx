0f0b05c94418a49477ff2693cdb37350a288bc7c
sohee <cherepanova.so04@gmail.com>
2025-08-18 18:28:39 +0500

    fix: heading for <880 only

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import useWindowWidth from '../../hooks/useWindowWidth';
 
 import { Button } from '../../uikit/Button/Button';
 import { Input } from '../../uikit/Input/Input';
@@ -12,6 +13,7 @@ import s from './styles.module.css';
 import { Header } from '../../components/Header/Header';
 
 const Test = () => {
+	const width = useWindowWidth();
 	const navigate = useNavigate();
 
 	const [checked, setChecked] = useState(false);
@@ -21,7 +23,7 @@ const Test = () => {
 
 	return (
 		<>
-			<Header heading="Test" onChevronClick={() => {}} />
+			{width < 880 && <Header heading="Test" onChevronClick={() => {}} />}
 			<main className={s.main}>
 				<section className={s.main__section}>
 					<UserInfo
