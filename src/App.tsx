9a6452135fb16a874fd7625844c1b4fee9932b18
asxnah <cherepanova.so04@gmail.com>
2025-10-22 16:13:23 +0500

    del: mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import useWindowWidth from './hooks/useWindowWidth';
 import { TabBar } from './components/TabBar/TabBar';
+import NotFound from './pages/NotFound/NotFound';
 import Test from './pages/Test/Test';
 import Auth from './pages/Auth/Auth';
 import Contacts from './pages/Contacts/Contacts';
 import Contact from './pages/mobile/Contact/Contact';
 import Chats from './pages/Chats/Chats';
-import NotFound from './pages/NotFound/NotFound';
 import BasicInfoFormPage from './pages/mobile/BasicInfoFormPage/BasicInfoFormPage';
 
 function App() {
-	const width = useWindowWidth();
 	return (
 		<BrowserRouter>
-			{width > 800 && <TabBar />}
+			<TabBar />
 			<Routes>
 				<Route path="/" element={<Test />} />
 				<Route path="/auth" element={<Auth />} />
