3e83ce2ce7360cb2b6b6581c17e498317f043ac8
asxnah <cherepanova.so04@gmail.com>
2025-10-22 16:34:50 +0500

    del: mobile

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import NotFound from './pages/NotFound/NotFound';
 import Test from './pages/Test/Test';
 import Auth from './pages/Auth/Auth';
 import Contacts from './pages/Contacts/Contacts';
-import Contact from './pages/mobile/Contact/Contact';
 import Chats from './pages/Chats/Chats';
-import BasicInfoFormPage from './pages/mobile/BasicInfoFormPage/BasicInfoFormPage';
 import './App.css';
 
 function App() {
@@ -17,10 +15,6 @@ function App() {
 				<Route path="/" element={<Test />} />
 				<Route path="/auth" element={<Auth />} />
 				<Route path="/contacts" element={<Contacts />} />
-				<Route path="/contacts/create" element={<BasicInfoFormPage />} />
-				<Route path="/contact/:id" element={<Contact />} />
-				<Route path="/contact/edit/:id" element={<BasicInfoFormPage />} />
-				<Route path="/settings/profile/edit" element={<BasicInfoFormPage />} />
 				<Route path="/chats" element={<Chats />} />
 
 				<Route path="*" element={<NotFound />} />
