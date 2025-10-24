85ae272087fa6c4f6af9df9783d1df54af337fd8
asuna <cherepanova.so04@gmail.com>
2025-10-25 01:35:20 +0500

    refactor: code optimization; using mock api; types moved into a separate file

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,228 +1,295 @@
-import { useEffect, useMemo, useState } from 'react';
-import { UserInfo } from '../../components/UserInfo/UserInfo';
-import { Button } from '../../uikit/Button/Button';
-import { Input } from '../../uikit/Input/Input';
-import { Toggler } from '../../uikit/Toggler/Toggler';
-import { AddContactIcon } from '../../assets/icons/AddContactIcon';
-import { Popup } from '../../components/Popup/Popup';
-import { Form } from '../../components/Form/Form';
-import s from './styles.module.css';
-
-import fetchedContacts from '../../mockData/contacts.json';
+import axios from "axios";
+import { useEffect, useMemo, useState } from "react";
+import type { Contact, FormData } from "./types";
+import { UserInfo } from "../../components/UserInfo/UserInfo";
+import { Button } from "../../uikit/Button/Button";
+import { Input } from "../../uikit/Input/Input";
+import { Toggler } from "../../uikit/Toggler/Toggler";
+import { AddContactIcon } from "../../assets/icons/AddContactIcon";
+import { Popup } from "../../components/Popup/Popup";
+import { Form } from "../../components/Form/Form";
+import s from "./styles.module.css";
 
 const Contacts = () => {
-	interface Contact {
-		id: string;
-		name: string;
-		avatar: string;
-		email: string;
-		notifs: boolean;
-	}
+  const [searchValue, setSearchValue] = useState<string>("");
+  const [contacts, setContacts] = useState<Contact[]>([]);
+  const [isPopupOpened, setPopupOpened] = useState<boolean>(false);
+  const [formHeading, setFormHeading] = useState<
+    "New contact" | "Edit contact"
+  >("New contact");
+  const [user, setUser] = useState<Contact>({
+    id: "",
+    name: "",
+    avatar: "",
+    email: "",
+    notifs: true,
+  });
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    email: "",
+  });
+  const filteredContacts = useMemo(() => {
+    return searchValue
+      ? contacts.filter((contact) =>
+          contact.name.toLowerCase().includes(searchValue.toLowerCase())
+        )
+      : contacts;
+  }, [searchValue, contacts]);
 
-	const [searchValue, setSearchValue] = useState<string>('');
-	const [contacts, setContacts] = useState<Contact[]>([]);
-	const [user, setUser] = useState<Contact>({
-		id: '',
-		name: '',
-		avatar: '',
-		email: '',
-		notifs: false,
-	});
-	const filteredContacts = useMemo(() => {
-		return searchValue
-			? contacts.filter((contact) =>
-					contact.name.toLowerCase().includes(searchValue.toLowerCase())
-			  )
-			: contacts;
-	}, [searchValue, contacts]);
-	const [formHeading, setFormHeading] = useState<
-		'New contact' | 'Edit contact'
-	>('New contact');
-	const [nameValue, setNameValue] = useState<string>('');
-	const [emailValue, setEmailValue] = useState<string>('');
-	const [isPopupOpened, setPopupOpened] = useState<boolean>(false);
+  const setNotifs = (id: string) => {
+    const updatedContacts = contacts.map((contact) =>
+      contact.id === id ? { ...contact, notifs: !contact.notifs } : contact
+    );
+    setContacts(updatedContacts);
 
-	useEffect(() => {
-		if (fetchedContacts.length > 0) {
-			setContacts(fetchedContacts);
-			setUser(fetchedContacts[0]);
-			setNameValue(fetchedContacts[0].name);
-			setEmailValue(fetchedContacts[0].email);
-		}
-	}, []);
+    if (user.id === id) {
+      const updatedUser = updatedContacts.find((contact) => contact.id === id);
+      if (updatedUser) setUser(updatedUser);
+    }
+  };
 
-	const search = (value: string) => {
-		setSearchValue(value);
-	};
+  const selectContact = (id: string) => {
+    const foundContact = contacts.find((contact) => contact.id === id);
 
-	const setNotifsGlobally = (id: string) => {
-		const updatedContacts = contacts.map((contact) =>
-			contact.id === id ? { ...contact, notifs: !contact.notifs } : contact
-		);
-		setContacts(updatedContacts);
+    if (!foundContact) return;
+    setUser(foundContact);
+  };
 
-		if (user.id === id) {
-			const updatedUser = updatedContacts.find((contact) => contact.id === id);
-			if (updatedUser) setUser(updatedUser);
-		}
-	};
+  const openPopup = (goal: "edit" | "add") => {
+    if (goal === "add") {
+      setFormHeading("New contact");
+    }
+    if (goal === "edit") {
+      setFormHeading("Edit contact");
+      setFormData({
+        name: user.name,
+        email: user.email,
+      });
+    }
+    setPopupOpened(true);
+  };
 
-	const contactAction = (id: string) => {
-		const foundContact = contacts.find((contact) => contact.id === id);
+  const submitPopup = async () => {
+    if (formHeading === "New contact") {
+      const newContact: Contact = {
+        id: Date.now().toString(),
+        name: formData.name,
+        avatar:
+          "https://i.pinimg.com/736x/6e/49/02/6e4902bf4606759e493020262a27aa68.jpg",
+        email: formData.email,
+        notifs: true,
+      };
 
-		if (!foundContact) return;
-		setUser(foundContact);
-	};
+      try {
+        await axios.post("http://localhost:3000/contacts", newContact);
+        setContacts([...contacts, newContact]);
+        setUser(newContact);
+        setFormData({
+          name: "",
+          email: "",
+        });
+        setPopupOpened(false);
+      } catch (err) {
+        console.error(err);
+      }
+    }
 
-	const submitPopup = () => {
-		if (formHeading === 'New contact') {
-			const newContact: Contact = {
-				id: Date.now().toString(), // placeholder => db search
-				name: nameValue,
-				avatar:
-					'https://i.pinimg.com/736x/e3/e4/63/e3e463274111bdfa9bcc2bdad6d51afe.jpg', // placeholder => db search
-				email: emailValue,
-				notifs: true,
-			};
-			setContacts([...contacts, newContact]);
-			setUser(newContact);
-			setNameValue('');
-			setEmailValue('');
-			setPopupOpened(false);
-		}
+    if (formHeading === "Edit contact") {
+      try {
+        await axios.patch(`http://localhost:3000/contacts/${user.id}`, {
+          name: formData.name,
+          email: formData.email,
+        });
 
-		if (formHeading === 'Edit contact') {
-			const updatedContacts = contacts.map((contact) =>
-				contact.id === user.id
-					? { ...contact, name: nameValue, email: emailValue }
-					: contact
-			);
-			setContacts(updatedContacts);
-			const updatedUser = updatedContacts.find(
-				(contact) => contact.id === user.id
-			);
-			if (updatedUser) setUser(updatedUser);
-			setPopupOpened(false);
-		}
-	};
+        const updatedContacts = contacts.map((contact) =>
+          contact.id === user.id
+            ? { ...contact, name: formData.name, email: formData.email }
+            : contact
+        );
+        setContacts(updatedContacts);
 
-	const addContact = () => {
-		setFormHeading('New contact');
-		setNameValue('');
-		setEmailValue('');
-		setPopupOpened(true);
-	};
+        const updatedUser = updatedContacts.find(
+          (contact) => contact.id === user.id
+        );
+        if (updatedUser) setUser(updatedUser);
 
-	const editContact = () => {
-		setFormHeading('Edit contact');
-		setNameValue(user.name);
-		setEmailValue(user.email);
-		setPopupOpened(true);
-	};
+        setFormData({
+          name: "",
+          email: "",
+        });
+        setPopupOpened(false);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  };
 
-	const deleteContact = (id: string) => {
-		const updatedContacts = contacts.filter((contact) => contact.id !== id);
-		setContacts(updatedContacts);
-		if (updatedContacts.length > 0) {
-			setUser(updatedContacts[0]);
-		}
-	};
+  const deleteContact = async (id: string) => {
+    try {
+      await axios.delete(`http://localhost:3000/contacts/${id}`);
 
-	return (
-		<>
-			<main className={s.main}>
-				{isPopupOpened && (
-					<Popup heading={formHeading} onClose={() => setPopupOpened(false)}>
-						<Form
-							buttonText="Save"
-							nameValue={nameValue}
-							emailValue={emailValue}
-							onNameChange={(e) => setNameValue(e.target.value)}
-							onEmailChange={(e) => setEmailValue(e.target.value)}
-							onSubmit={submitPopup}
-						/>
-					</Popup>
-				)}
-				<section className={s.contactsList}>
-					<div className={s.searchbar}>
-						<Button
-							onClick={addContact}
-							content={<AddContactIcon color="#fcfcfc" />}
-						/>
-						<Input
-							name="search"
-							placeholder="Search contacts"
-							value={searchValue}
-							onChange={(e) => search(e.target.value)}
-						/>
-					</div>
-					{contacts.length > 0 ? (
-						filteredContacts.map((contact) => {
-							return (
-								<UserInfo
-									key={contact.id}
-									type="contact"
-									id={contact.id}
-									name={contact.name}
-									avatar={contact.avatar}
-									selected={user.id === contact.id}
-									onClick={() => contactAction(contact.id)}
-								/>
-							);
-						})
-					) : (
-						<div className={s.noContacts}>
-							<p>
-								You don’t have contacts yet.&nbsp;{' '}
-								<button className={s.noContacts__button} onClick={addContact}>
-									Create first contact
-								</button>
-							</p>
-						</div>
-					)}
-				</section>
-				<section className={s.profileTab}>
-					{contacts.length > 0 ? (
-						<>
-							<UserInfo
-								type="profile"
-								name={user.name}
-								avatar={user.avatar}
-								content={user.email}
-								onClick={editContact}
-							/>
-							<ul className={s.profileTab__list}>
-								<li key="message">
-									<UserInfo
-										type="link"
-										name="Write a message"
-										onClick={() => console.log('Write a message')}
-									/>
-								</li>
-								<li key="notifs">
-									<Toggler
-										content="Notifications"
-										checked={user.notifs}
-										onToggle={() => setNotifsGlobally(user.id)}
-									/>
-								</li>
-							</ul>
-							<button
-								className={s.profileTab__button}
-								onClick={() => deleteContact(user.id)}
-							>
-								Delete contact
-							</button>
-						</>
-					) : (
-						<p className={s.profileTab__noSelection}>
-							Select a chat or a contact to start messaging
-						</p>
-					)}
-				</section>
-			</main>
-		</>
-	);
+      const updatedContacts = contacts.filter((contact) => contact.id !== id);
+      setContacts(updatedContacts);
+
+      if (updatedContacts.length > 0) {
+        setUser(updatedContacts[0]);
+      } else {
+        setUser({
+          id: "",
+          name: "",
+          avatar: "",
+          email: "",
+          notifs: true,
+        });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get("http://localhost:3000/contacts");
+        if (Array.isArray(data) && data.length) {
+          setContacts(data);
+          setUser(data[0]);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, []);
+
+  return (
+    <>
+      <main className={s.main}>
+        {isPopupOpened && (
+          <Popup heading={formHeading} onClose={() => setPopupOpened(false)}>
+            <Form
+              buttonText="Save"
+              nameValue={formData.name}
+              emailValue={formData.email}
+              onNameChange={(e) =>
+                setFormData((prev) => ({ ...prev, name: e.target.value }))
+              }
+              onEmailChange={(e) =>
+                setFormData((prev) => ({ ...prev, email: e.target.value }))
+              }
+              onSubmit={submitPopup}
+            />
+          </Popup>
+        )}
+        <section className={s.contactsList}>
+          <div className={s.searchbar}>
+            <Button
+              onClick={() => openPopup("add")}
+              content={<AddContactIcon color="#fcfcfc" />}
+            />
+            <Input
+              name="search"
+              placeholder="Search contacts"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+            />
+          </div>
+          {contacts.length > 0 ? (
+            filteredContacts.map((contact) => {
+              return (
+                <UserInfo
+                  key={contact.id}
+                  type="contact"
+                  id={contact.id}
+                  name={contact.name}
+                  avatar={contact.avatar}
+                  selected={user.id === contact.id}
+                  onClick={() => selectContact(contact.id)}
+                />
+              );
+            })
+          ) : (
+            <div className={s.noContacts}>
+              <p>
+                You don’t have contacts yet.&nbsp;{" "}
+                <button
+                  className={s.noContacts__button}
+                  onClick={() => openPopup("add")}
+                >
+                  Create first contact
+                </button>
+              </p>
+            </div>
+          )}
+          {contacts.length > 0 ? (
+            filteredContacts.map((contact) => {
+              return (
+                <UserInfo
+                  key={contact.id}
+                  type="contact"
+                  id={contact.id}
+                  name={contact.name}
+                  avatar={contact.avatar}
+                  selected={user.id === contact.id}
+                  onClick={() => selectContact(contact.id)}
+                />
+              );
+            })
+          ) : (
+            <div className={s.noContacts}>
+              <p>
+                You don’t have contacts yet.&nbsp;{" "}
+                <button
+                  className={s.noContacts__button}
+                  onClick={() => openPopup("add")}
+                >
+                  Create first contact
+                </button>
+              </p>
+            </div>
+          )}
+        </section>
+        <section className={s.profileTab}>
+          {contacts.length > 0 ? (
+            <>
+              <UserInfo
+                type="profile"
+                name={user.name}
+                avatar={user.avatar}
+                content={user.email}
+                onClick={() => openPopup("edit")}
+              />
+              <ul className={s.profileTab__list}>
+                <li key="message">
+                  <UserInfo
+                    type="link"
+                    name="Write a message"
+                    onClick={() => console.log("Write a message")}
+                  />
+                </li>
+                <li key="notifs">
+                  <Toggler
+                    content="Notifications"
+                    checked={user.notifs}
+                    onToggle={() => setNotifs(user.id)}
+                  />
+                </li>
+              </ul>
+              <button
+                className={s.profileTab__button}
+                onClick={() => deleteContact(user.id)}
+              >
+                Delete contact
+              </button>
+            </>
+          ) : (
+            <p className={s.profileTab__noSelection}>
+              Select a chat or a contact to start messaging
+            </p>
+          )}
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default Contacts;
---
+++ src/pages/Contacts/types.ts
@@ -0,0 +1,12 @@
+export interface Contact {
+  id: string;
+  name: string;
+  avatar: string;
+  email: string;
+  notifs: boolean;
+}
+
+export interface FormData {
+  name: string;
+  email: string;
+}
