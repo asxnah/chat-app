b3a02c3ff5fa55a15b4fb049bf2ee9a0f4f1834a
asuna <cherepanova.so04@gmail.com>
2025-10-27 17:17:23 +0500

    refactor: formatting

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -4,45 +4,45 @@ import { Input } from '../../uikit/Input/Input';
 import s from './styles.module.css';
 
 interface FormProps {
-	buttonText: string;
-	nameValue: string;
-	emailValue: string;
-	onNameChange: (e: ChangeEvent<HTMLInputElement>) => void;
-	onEmailChange: (e: ChangeEvent<HTMLInputElement>) => void;
-	onSubmit: (e: FormEvent<HTMLFormElement>) => void;
+  buttonText: string;
+  nameValue: string;
+  emailValue: string;
+  onNameChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onEmailChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onSubmit: (e: FormEvent<HTMLFormElement>) => void;
 }
 
 export const Form = ({
-	buttonText,
-	nameValue,
-	emailValue,
-	onNameChange,
-	onEmailChange,
-	onSubmit,
+  buttonText,
+  nameValue,
+  emailValue,
+  onNameChange,
+  onEmailChange,
+  onSubmit,
 }: FormProps) => {
-	const submit = (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		onSubmit(e);
-	};
+  const submit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit(e);
+  };
 
-	return (
-		<form className={s.form} onSubmit={submit}>
-			<div className={s.form__inputs}>
-				<Input
-					name="name"
-					placeholder="Name"
-					value={nameValue}
-					onChange={onNameChange}
-				/>
-				<Input
-					name="email"
-					type="email"
-					placeholder="email@example.com"
-					value={emailValue}
-					onChange={onEmailChange}
-				/>
-			</div>
-			<Button type="submit" content={buttonText} />
-		</form>
-	);
+  return (
+    <form className={s.form} onSubmit={submit}>
+      <div className={s.form__inputs}>
+        <Input
+          name='name'
+          placeholder='Name'
+          value={nameValue}
+          onChange={onNameChange}
+        />
+        <Input
+          name='email'
+          type='email'
+          placeholder='email@example.com'
+          value={emailValue}
+          onChange={onEmailChange}
+        />
+      </div>
+      <Button type='submit' content={buttonText} />
+    </form>
+  );
 };
--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -3,23 +3,23 @@ import { CrossIcon } from '../../assets/icons/CrossIcon';
 import s from './styles.module.css';
 
 interface PopupProps {
-	heading: string;
-	children: ReactNode;
-	onClose: () => void;
+  heading: string;
+  children: ReactNode;
+  onClose: () => void;
 }
 
 export const Popup = ({ heading, children, onClose }: PopupProps) => {
-	return (
-		<section className={s.wrapper}>
-			<div className={s.form}>
-				<div className={s.heading}>
-					<h3 className={s.heading__title}>{heading}</h3>
-					<button onClick={onClose} type="button">
-						<CrossIcon />
-					</button>
-				</div>
-				{children}
-			</div>
-		</section>
-	);
+  return (
+    <section className={s.wrapper}>
+      <div className={s.form}>
+        <div className={s.heading}>
+          <h3 className={s.heading__title}>{heading}</h3>
+          <button onClick={onClose} type='button'>
+            <CrossIcon />
+          </button>
+        </div>
+        {children}
+      </div>
+    </section>
+  );
 };
